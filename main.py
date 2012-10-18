df771659b9cbec6fec223ec9340c4feb5ebb9e84
DBProductions <dennis@dbproductions.de>
2012-10-18 21:28:18 +0200

    init commit, add first files

---
+++ main.py
@@ -0,0 +1,19 @@
+import webapp2
+from google.appengine.ext import db
+
+class Trackentry(db.Model):
+    url = db.StringProperty(required=True)
+    site = db.StringProperty(required=True)
+    requested = db.DateTimeProperty(auto_now_add=True)
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        url = self.request.get('url')
+        site = self.request.get('site')
+        if site and url:
+            t = Trackentry(url = url, site = site)
+            t.put()
+        self.response.headers["Content-Type"] = "image/gif"
+        #self.response.write()
+
+app = webapp2.WSGIApplication([('/', MainHandler)])
